76525d533d55789d976c0455d2d4e65dcf9df030
Mir Sadab Ali <alisadab404@gmail.com>
2026-01-20 15:35:03 +0530

    mondo db setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Mir Sadab Ali",
+    author_email="alisadab404@gmail.com",
+    packages=find_packages()
+)
