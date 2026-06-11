98f3603992a66ca8dec445c9f04b2a26d319832c
Mir Sadab Ali <alisadab404@gmail.com>
2026-06-11 09:16:40 +0530

    CICD

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -74,7 +74,7 @@ MIN_SAMPLES_SPLIT_RANDOM_STATE: int = 101
 MODEL Evaluation related constants
 """
 MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
-MODEL_BUCKET_NAME = "ml-veivhel"
+MODEL_BUCKET_NAME = "my-model-mlo"
 MODEL_PUSHER_S3_KEY = "model-registry"
 
 
